5737f1c81a6e15cf02afc9a3e0f8aa747b0b184b
Ignatus <ignatusa3@gmail.com>
2025-03-20 09:34:08 +0000

    chore: update sns roles

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -169,63 +169,6 @@ resource "aws_cloudwatch_metric_alarm" "ecs_memory_alarm" {
   }
 }
 
-# RDS CPU Utilization Alarm
-resource "aws_cloudwatch_metric_alarm" "rds_cpu_alarm" {
-  alarm_name          = "${var.project_name}-rds-cpu-alarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/RDS"
-  period              = 60
-  statistic           = "Average"
-  threshold           = 80
-  alarm_description   = "This alarm monitors RDS CPU utilization"
-  alarm_actions       = [aws_sns_topic.cloudwatch_alarms.arn]
-  ok_actions          = [aws_sns_topic.cloudwatch_alarms.arn]
-
-  dimensions = {
-    DBInstanceIdentifier = aws_db_instance.lamp_db.id
-  }
-}
-
-# RDS Free Storage Space Alarm
-resource "aws_cloudwatch_metric_alarm" "rds_storage_alarm" {
-  alarm_name          = "${var.project_name}-rds-storage-alarm"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = 2
-  metric_name         = "FreeStorageSpace"
-  namespace           = "AWS/RDS"
-  period              = 60
-  statistic           = "Average"
-  threshold           = 5000000000 # 5GB in bytes
-  alarm_description   = "This alarm monitors RDS free storage space"
-  alarm_actions       = [aws_sns_topic.cloudwatch_alarms.arn]
-  ok_actions          = [aws_sns_topic.cloudwatch_alarms.arn]
-
-  dimensions = {
-    DBInstanceIdentifier = aws_db_instance.lamp_db.id
-  }
-}
-
-# ALB 5XX Error Count Alarm
-resource "aws_cloudwatch_metric_alarm" "alb_5xx_alarm" {
-  alarm_name          = "${var.project_name}-alb-5xx-alarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
-  metric_name         = "HTTPCode_Target_5XX_Count"
-  namespace           = "AWS/ApplicationELB"
-  period              = 60
-  statistic           = "Sum"
-  threshold           = 10
-  alarm_description   = "This alarm monitors ALB 5XX errors"
-  alarm_actions       = [aws_sns_topic.cloudwatch_alarms.arn]
-  ok_actions          = [aws_sns_topic.cloudwatch_alarms.arn]
-
-  dimensions = {
-    LoadBalancer = aws_lb.lamp_alb.arn_suffix
-  }
-}
-
 # Output for monitoring resources
 output "cloudwatch_dashboard_url" {
   value = "https://${var.aws_region}.console.aws.amazon.com/cloudwatch/home?region=${var.aws_region}#dashboards:name=${var.project_name}-dashboard"
--- sns.tf
+++ sns.tf
@@ -9,3 +9,40 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol  = "email"
   endpoint  = "ignatusa3@gmail.com"
 }
+
+# IAM Role for CloudWatch to publish to SNS
+resource "aws_iam_role" "cloudwatch_sns_role" {
+  name = "${var.project_name}-cloudwatch-sns-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudwatch.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for CloudWatch to publish to SNS
+resource "aws_iam_role_policy" "cloudwatch_sns_policy" {
+  name = "${var.project_name}-cloudwatch-sns-policy"
+  role = aws_iam_role.cloudwatch_sns_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sns:Publish"
+        ]
+        Effect   = "Allow"
+        Resource = aws_sns_topic.cloudwatch_alarms.arn
+      }
+    ]
+  })
+}
